77781cae39cc7004e4f6984106be5d49ee183166
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-09 01:43:25 +0530

    reducer function made

--- src/store.js
+++ src/store.js
@@ -4,9 +4,28 @@ const initialState = {
   loanPurpose: "",
 };
 
-function reducer(){
-  switch(action.type){
+function reducer(state = initialState, action) {
+  switch (action.type) {
     case "account/deposit":
-      return {...}
+      return { ...state, balance: state.balance + action.payload };
+
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+
+    case "account/requestLoan":
+      if (state.loan > 0) return state; //current state
+      return {
+        ...state,
+        loan: action.payload, // this thing is not right
+      };
+
+    case "account/payloan":
+      return {
+        ...state,
+        loan: 0,
+        balance: state.balance - action.payload,
+      };
+    default:
+      return state; // in reducer function in redux we retrun state not throw error like in useReducer function reducer
   }
 }
