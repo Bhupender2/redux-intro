d3d41834b726258377d268eba857f8ed99f67840
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-10 08:41:10 +0530

    small description about how to connect redux from react store

--- src/store.js
+++ src/store.js
@@ -10,3 +10,5 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer); // we are creating the store here and the reducer store receives are always called root reducers
 
 export default store  // importing the store in our application
+
+//connecting redux from our react app using react-redux library and use provider componnet to wrap our whole app inside the provider component and pass store as a prop now every component inside provide can read data from store and dispatch action to it and to read data fom store we use useSelector hook from react-redux library while takes store as an argumnent 
