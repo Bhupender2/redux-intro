9b54dbc4d051e1995d36a9e73f6929f5b3b87a1c
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-13 20:01:24 +0530

    redux toolkit added and store.js copy made

---
+++ src/store-v2.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from "redux"; //combining all the reducer to create root reducer;
+import accountReducer from "./features/accounts/accountSlice";
+import costumerReducer from "./features/customers/customerSlice";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const rootReducer = combineReducers({
+  account: accountReducer, // giving reducer a meaningfull name
+  customer: costumerReducer,
+});
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk))); // we are creating the store here and the reducer store receives are always called root reducers and we wrap our middleware inside composeWithDevTools
+
+export default store; // importing the store in our application
+
+//connecting redux from our react app using react-redux library and use provider componnet to wrap our whole app inside the provider component and pass store as a prop now every component inside provide can read data from store and dispatch action to it and to read data fom store we use useSelector hook from react-redux library while takes store as an argumnent.
--- src/store.js
+++ src/store.js
@@ -1,16 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"; //combining all the reducer to create root reducer;
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import costumerReducer from "./features/customers/customerSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({
-  account: accountReducer, // giving reducer a meaningfull name
-  customer: costumerReducer,
-});
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk))); // we are creating the store here and the reducer store receives are always called root reducers
+const store = configureStore({ //here using configure store makes devtools , combining reducers , applying middleware thunk automatically
+  reducer: {
+    account: accountReducer,
+    customer: costumerReducer,
+  },
+}); // we are creating the store here and the reducer store receives are always called root reducers and we wrap our middleware inside composeWithDevTools
 
 export default store; // importing the store in our application
-
-//connecting redux from our react app using react-redux library and use provider componnet to wrap our whole app inside the provider component and pass store as a prop now every component inside provide can read data from store and dispatch action to it and to read data fom store we use useSelector hook from react-redux library while takes store as an argumnent.
